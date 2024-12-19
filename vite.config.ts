08e2bd3bcac554b4eb6a87b817fae30d1612ca26
ali <muhammadalihassanofficial@gmail.com>
2024-12-19 16:28:07 +0500

    chore(vitest): add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@/app": "/src/app",
       "@/components": "/src/components",
-      "@/hooks": "/src/hooks"
+      "@/hooks": "/src/hooks",
     },
   },
 });
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
