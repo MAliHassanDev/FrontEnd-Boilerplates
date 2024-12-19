8f834426b4da25fd20f5829784dec626c4370517
ali <muhammadalihassanofficial@gmail.com>
2024-12-19 17:59:41 +0500

    test: button component

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Button from "./Button";
+import ThemeContext, {
+  type ThemeContextType,
+} from "@/hooks/context/themeContext";
+
+describe("Button", () => {
+  it("renders with text which includes opposite theme", () => {
+    renderButtonWithContext({
+      theme: "dark",
+      toggleTheme: vi.fn(),
+    });
+    const button = screen.getByTestId("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/Switch to light mode/i);
+  });
+
+  it("calls toggleTheme when clicked", () => {
+    const toggleTheme = vi.fn();
+    renderButtonWithContext({ theme: "dark", toggleTheme });
+    fireEvent.click(screen.getByTestId("button"));
+    expect(toggleTheme).toHaveBeenCalledOnce();
+  });
+});
+
+function renderButtonWithContext({ theme, toggleTheme }: ThemeContextType) {
+  render(
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      <Button />
+    </ThemeContext.Provider>,
+  );
+}
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ import styles from "@/components/button/Button.module.css";
 const Button = () => {
   const { theme, toggleTheme } = useContext(ThemeContext) as ThemeContextType;
   return (
-    <button className={styles.button} onClick={toggleTheme}>
+    <button className={styles.button} onClick={toggleTheme} data-testid="button">
       Switch to {theme == "dark" ? "light" : "dark"} mode
     </button>
   );
