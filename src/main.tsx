3f74925333a29e7f839ea94899077bf4f9d821d5
ali <muhammadalihassanofficial@gmail.com>
2024-12-22 14:54:19 +0500

    chore: add material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./styles/main.css";
+
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "@/app/App";
-import "./styles/main.css";
 import { ThemeProvider } from "@/components/themeProvider/ThemeProvider";
 
 const rootElement = document.getElementById("root");
