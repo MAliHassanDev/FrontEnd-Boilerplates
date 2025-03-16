73f1b6f92923301f1e9e74aeafcfd4e9c079060b
ali <muhammadalihassanofficial@gmail.com>
2025-03-16 15:10:43 +0500

    feat: add zaplog as app logger

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
+  RouterProvider,
 } from "react-router";
 import { ToastContainer } from "react-toastify";
 import { Home } from "@/pages/home/HomePage";
@@ -10,22 +10,25 @@ import { SigninPage } from "@/features/auth/pages/SigninPage";
 import { NotFoundPage } from "@/pages/notfound/NotFoundPage";
 import { RequireAuth } from "@/features/auth/components/RequireAuth";
 import { RootLayout } from "@/common/layouts/root/RootLayout";
+import { logger } from "@/lib/logger";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route  element={<RequireAuth />}>
+      <Route element={<RequireAuth />}>
         <Route path="/" element={<Home />} />
       </Route>
       <Route path="login" element={<SigninPage />} />
       {/* <Route path="register" element={<RegisterPage />} /> */}
 
       <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
+    </Route>,
+  ),
 );
 
 const App = () => {
+  logger.info("App is running", "App");
+
   return (
     <div className="">
       <ToastContainer />
